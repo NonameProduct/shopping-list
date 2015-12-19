57bb7f01848ffbbb03503c9a8ed4c32cac4afd29
Till <itsmytrash@web.de>
2015-12-19 12:56:05 +0100

    Added option in menue to delete the list.

--- app/src/main/java/com/example/till/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/till/shoppinglist/MainActivity.java
@@ -41,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.delete_list) {
+            shopping_list_items = "";
             return true;
         }
 
